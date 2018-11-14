e4f5723a6930368ca73404250ffe50a560d4bf33
ron litzenberger <ron.litzenberger@nearform>
2018-11-14 08:06:35 -0600

    Benchmark: adding ipfs extract benchmark using unixFS engine

---
+++ tests/local-extract.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+'use strict'
+
+const fs = require('fs')
+const prettyHrtime = require('pretty-hrtime')
+const IPFS = require('ipfs')
+const ora = require('ora')
+const handler = "local extract"
+
+
+
+const node = new Promise((resolve) => {
+  const config = {
+    "Addresses": {
+      "API": "/ip4/127.0.0.1/tcp/5012",
+      "Gateway": "/ip4/127.0.0.1/tcp/9091",
+      "Swarm": [
+        "/ip4/0.0.0.0/tcp/4012",
+        "/ip4/127.0.0.1/tcp/4013/ws"
+      ]
+    },
+    "Bootstrap": []
+  }
+  const peer = new IPFS({
+    repo: '/tmp/peer',
+    config: config,
+    init: {
+      emptyRepo: true
+    }
+  })
+  peer.on('ready', () => {
+    console.log('node ready')
+    resolve(peer)
+  })
+})
+
+const run = async () => {
+  const spinner = ora(`Started ${handler}`).start()
+  spinner.color = 'magenta'
+  spinner.text = "testing"
+  const node1 = await node
+  const fileStream = fs.createReadStream("fixtures/200Bytes.txt")
+  const inserted = await node1.files.add(fileStream)
+  console.log('vmx: inserted:', inserted)
+  const validCID = inserted[0].hash
+  node1.files.get(validCID, function (err, files) {
+    files.forEach((file) => {
+      console.log(file.path)
+      console.log(file.content.toString('utf8'))
+      spinner.succeed()
+      process.exit(1)
+    })
+  })
+
+}
+run()
