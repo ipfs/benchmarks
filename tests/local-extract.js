e769575d967d9d20ad6bd9946df30a40a62f9428
ron litzenberger <ron.litzenberger@nearform>
2018-11-15 14:23:01 -0600

    Remove extra resolve

--- tests/local-extract.js
+++ tests/local-extract.js
@@ -9,35 +9,33 @@ const node = require('../lib/create-node.js')
 const ora = require('ora')
 const handler = "local extract"
 
-function localExtract(logger, name, file) {
-  return new Promise(async (resolve) => {
-    const spinner = ora(`Started ${handler}`).start()
-    spinner.color = 'magenta'
-    spinner.text = "testing"
-    const node1 = await node
-    const fileStream = fs.createReadStream(file)
-    const inserted = await node1.files.add(fileStream)
-    const start = process.hrtime();
-    const validCID = inserted[0].hash
-    const files = await node1.files.get(validCID)
-    const end = process.hrtime(start);
-    files.forEach((file) => {
-      logger.info(file.path)
+async function localExtract(logger, name, file) {
+  const spinner = ora(`Started ${handler}`).start()
+  spinner.color = 'magenta'
+  spinner.text = "testing"
+  const node1 = await node
+  const fileStream = fs.createReadStream(file)
+  const inserted = await node1.files.add(fileStream)
+  const start = process.hrtime();
+  const validCID = inserted[0].hash
+  const files = await node1.files.get(validCID)
+  const end = process.hrtime(start);
+  files.forEach((file) => {
+    logger.info(file.path)
 
-    })
-    await spinner.succeed()
-    // await node1.stop()
-    const d = new Date()
-    resolve(
-      {
-        name: name,
-        file: file,
-        date: d.toISOString(),
-        s: end[0],
-        ms: end[1] / 1000000
-      }
-    )
   })
+  await spinner.succeed()
+  // await node1.stop()
+  const d = new Date()
+  return (
+    {
+      name: name,
+      file: file,
+      date: d.toISOString(),
+      s: end[0],
+      ms: end[1] / 1000000
+    }
+  )
 
 }
 
