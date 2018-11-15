fde0fd80e50cb1d452960b9c841381ed86f6b676
ron litzenberger <ron.litzenberger@nearform>
2018-11-14 23:53:37 -0600

    local-extract: add timer

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ const os = require('os')
 
 // Run through use cases
 const logger = require('pino')()
-logger.info(os.cpus())
-
+const results = []
 async function benchmark() {
 
-  await localExtract(logger)
-  console.log("done")
-  logger.info(os.loadavg())
+  results.push(await localExtract(logger, "unixFS:extract:smallfile", "tests/fixtures/200Bytes.txt"))
+  results[0].cpu = os.cpus(),
+    results[0].loadAvg = os.loadavg()
+
+  results.push(await localExtract(logger, "unixFS:extract:largefile", "tests/fixtures/1.2MiB.txt"))
+  results[1].cpu = os.cpus(),
+    results[1].loadAvg = os.loadavg()
+  console.log(results)
 
 }
 benchmark()
--- tests/local-extract.js
+++ tests/local-extract.js
@@ -9,25 +9,34 @@ const node = require('../lib/create-node.js')
 const ora = require('ora')
 const handler = "local extract"
 
-function localExtract(logger) {
+function localExtract(logger, name, file) {
   return new Promise(async (resolve) => {
     const spinner = ora(`Started ${handler}`).start()
     spinner.color = 'magenta'
     spinner.text = "testing"
     const node1 = await node
-    const fileStream = fs.createReadStream("tests/fixtures/200Bytes.txt")
+    const fileStream = fs.createReadStream(file)
     const inserted = await node1.files.add(fileStream)
-    logger.info('inserted:', inserted)
+    const start = process.hrtime();
     const validCID = inserted[0].hash
     const files = await node1.files.get(validCID)
-
+    const end = process.hrtime(start);
     files.forEach((file) => {
       logger.info(file.path)
 
     })
     await spinner.succeed()
-    await node1.stop()
-    resolve()
+    // await node1.stop()
+    const d = new Date()
+    resolve(
+      {
+        name: name,
+        file: file,
+        date: d.toISOString(),
+        s: end[0],
+        ms: end[1] / 1000000
+      }
+    )
   })
 
 }
