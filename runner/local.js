74ddb035e04a9a06eac3f70c0c86c3e3c755a554
Elexy <alex@seretalabs.com>
2018-11-19 10:23:10 +0100

    util.promisify is great!

--- runner/local.js
+++ runner/local.js
@@ -1,20 +1,13 @@
 'use strict'
 
 const { exec } = require('child_process')
+const util = require('util')
 
 const config = require('./config')
 
 const run = shell => {
   config.log.info(`Running [${shell}] locally`)
-  return new Promise((resolve, reject) => {
-    exec(shell, (err, stdout, stderr) => {
-      if (err) {
-        reject(new Error(stderr))
-        return
-      }
-      resolve(stdout)
-    })
-  })
+  util.promisify(exec)
 }
 
 module.exports = {
