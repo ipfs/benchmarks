9e676af9b9d1e68a47081c355f3c3f9bd05f8707
ron litzenberger <ron.litzenberger@nearform>
2018-11-20 02:09:15 -0600

    Make lint happy

--- tests/lib/output.js
+++ tests/lib/output.js
@@ -5,33 +5,28 @@ const fs = require('fs')
 const Ajv = require('ajv')
 const schema = require('../schema/results.js')
 
-const folder = "out"
-async function write(data) {
-
-
+const folder = 'out'
+async function write (data) {
   const name = createFilename(data)
 
   if (validate(data)) {
     try {
       fs.writeFileSync(`${name}.json`, JSON.stringify(data))
       console.log(data)
-    }
-    catch (e) {
+    } catch (e) {
       throw new Error(e)
     }
   }
-  return
-
 }
 
-function createFilename(data) {
+function createFilename (data) {
   if (!fs.existsSync(folder)) {
-    fs.mkdirSync(folder);
+    fs.mkdirSync(folder)
   }
   return `${folder}/${data.name}-${new Date().toISOString()}`
 }
 
-function validate(data) {
+function validate (data) {
   const ajv = new Ajv({ useDefaults: true })
   const validate = ajv.compile(schema.valueOf())
 
