5914ccf6819a88aadbec2d5ddf1e3d0ec343b82d
Elexy <alex@seretalabs.com>
2018-11-26 22:27:25 +0100

    Revert "provision VM"
    This reverts commit 417ddec690a36ae53f3af8dd1824911ea7d70297.

--- runner/index.js
+++ runner/index.js
@@ -1,7 +1,6 @@
 'use strict'
 
 require('make-promises-safe') // installs an 'unhandledRejection' handler
-const schedule = require('node-schedule')
 const config = require('./config')
 const remote = require('./remote.js')
 const local = require('./local.js')
@@ -31,7 +30,4 @@ const main = async () => {
   }
 }
 
-// run this every day at midnight, at least
-schedule.scheduleJob('0 0 * * *', function () {
-  main()
-})
+main()
