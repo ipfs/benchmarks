840772348c15a4a43bccdba8a8685d72857182fa
Elexy <alex@seretalabs.com>
2018-11-29 20:56:47 +0100

    more flexible

--- scripts/runProdEnv.sh
+++ scripts/runProdEnv.sh
@@ -4,4 +4,4 @@ MODE=prod
 SCRIPTPATH=$(dirname "$0")
 source $SCRIPTPATH/common.sh
 
-docker-compose -f $SCRIPTPATH/../infrastructure/deploy/docker-compose.yaml -f $SCRIPTPATH/../infrastructure/deploy/docker-compose.prod.yaml $OP -d
+docker-compose -f $SCRIPTPATH/../infrastructure/deploy/docker-compose.yaml -f $SCRIPTPATH/../infrastructure/deploy/docker-compose.prod.yaml $OP
