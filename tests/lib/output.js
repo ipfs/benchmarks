76448ed37cc1ed7c594e58d787233c43d2cd15c1
ron litzenberger <ron.litzenberger@nearform>
2018-12-03 11:33:25 -0500

    Make lint happy

--- tests/lib/output.js
+++ tests/lib/output.js
@@ -18,9 +18,8 @@ async function store (data) {
     } else {
       console.log('Writing output in a multiple files')
       var table = new Table({
-        head: ['Test', 'Sub Test', 'Description', 'Test Class', 'Duration']
-      , colWidths: [20, 20, 50, 20]
-    })
+        head: ['Test', 'Sub Test', 'Description', 'Test Class', 'Duration'], colWidths: [20, 20, 50, 20]
+      })
       for (let testResult of data) {
         table.push([testResult.name, testResult.subtest, testResult.description, testResult.testClass, `s:${testResult.duration.s} ms: ${testResult.duration.ms}`])
         write(testResult)
