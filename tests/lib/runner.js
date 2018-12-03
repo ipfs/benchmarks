19532b38c143f044cb6e10d4cb3655a1dcf33f7f
ron litzenberger <ron.litzenberger@nearform>
2018-12-03 18:58:29 -0500

    Added env variablesto filter by subTest

--- tests/lib/runner.js
+++ tests/lib/runner.js
@@ -4,6 +4,7 @@ const config = require('../config')
 const clean = require('./clean')
 const { store } = require('./output')
 const testClassParam = process.env.TESTCLASS || false
+const subTestParam = process.env.SUBTEST || false
 async function runner (test) {
   const arrResults = []
   const nodeFactory = new NodeFactory()
@@ -14,19 +15,24 @@ async function runner (test) {
   node.push(await nodeFactory.add())
   node.push(await nodeFactory.add())
   for (let subTest of config[test.name]) {
+    if (subTestParam && subTest.subTest === subTestParam) {
+      for (let testClass of subTest.testClass) {
+        if (testClassParam && testClass === testClassParam) {
+          arrResults.push(await test(node, test.name, subTest.subTest, testClass))
+        } else if (!testClassParam) {
+          arrResults.push(await test(node, test.name, subTest.subTest, testClass))
+        }
+      }
+    } else if (!subTestParam) {      
     for (let testClass of subTest.testClass) {
       if (testClassParam && testClass === testClassParam) {
         arrResults.push(await test(node, test.name, subTest.subTest, testClass))
       } else if (!testClassParam) {
-        try{
-          console.log(`${test.name} ${subTest.subTest} ${testClass}`)
         arrResults.push(await test(node, test.name, subTest.subTest, testClass))
-        } catch(e){
-          console.log(e)
-        }
       }
     }
   }
+}
   store(arrResults)
   nodeFactory.stop()
   clean.peerRepos()
