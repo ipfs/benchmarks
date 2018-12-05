65d1e8fc132d638ed401f5d46eb51ad38ceaeb4a
Elexy <alex@seretalabs.com>
2018-12-05 12:20:28 +0100

    cleanup test and fix typo

--- tests/local-transfer.js
+++ tests/local-transfer.js
@@ -6,25 +6,24 @@ const { build } = require('./schema/results')
 const run = require('./lib/runner')
 
 const localTransfer = async (node, name, subTest, testClass, version) => {
-  // Insert into peerA
+  console.log(testClass)
   const fileStream = fs.createReadStream(fixtures[testClass])
   const peerA = node[0]
   const peerB = node[1]
   const peerAId = await peerA.id()
   peerB.swarm.connect(peerAId.addresses[0])
   const inserted = await peerA.files.add(fileStream)
-
-  // peerB doesn't have any data cached, get all from peerA
   const start = process.hrtime()
   await peerB.files.cat(inserted[0].hash)
   const end = process.hrtime(start)
 
   return build({
     name: name,
-    subtest: subTest,
-    testClass: testClass,
+    subTest: subTest,
     file: fixtures[testClass],
     meta: { version: version },
+    description: 'Transfer file between two local nodes',
+    testClass: testClass,
     duration: {
       s: end[0],
       ms: end[1] / 1000000
