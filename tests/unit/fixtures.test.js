d27b7a6cefe2443ae4b0e4fc6dbb3a10348b3a6d
ron litzenberger <ron.litzenberger@nearform>
2018-12-07 17:11:59 -0600

    Fix file set test

--- tests/unit/fixtures.test.js
+++ tests/unit/fixtures.test.js
@@ -34,7 +34,7 @@ test('Is a Directory ', async t => {
 test('File set ', async t => {
   t.plan(2)
   const results = await file('Hundred1KBFile')
-  console.log(results[0])
-  t.assert(results.length === 101)
+  console.log(results.length)
+  t.assert(results.length === 100)
   t.pass()
 })
