f8524f968b73112199e79d97f058253160a89916
ron litzenberger <ron.litzenberger@nearform>
2018-12-14 07:35:48 -0600

    Fix tests

--- tests/util/get-commit.js
+++ tests/util/get-commit.js
@@ -1,20 +1,18 @@
 'use strict'
 
-const path = require('path')
 const util = require('util')
 const execute = util.promisify(util.promisify(require('child_process').exec))
-var appDir = path.dirname(require.main.filename)
 
 const shell = async (command) => {
   const out = await execute(command)
   return out
 }
 const getIpfsCommit = async () => {
-  const out = await shell(`${appDir}/util/getCommit.sh ${appDir}/../../`)
+  const out = await shell(`${__dirname}/getCommit.sh ${__dirname}/../../`)
   return out.stdout.replace(/\n$/, '')
 }
 const getBranchName = async () => {
-  const out = await shell(`${appDir}/util/getBranch.sh ${appDir}/../../`)
+  const out = await shell(`${__dirname}/getBranch.sh ${__dirname}/../../`)
   return out.stdout.replace(/\n$/, '')
 }
 module.exports = {
