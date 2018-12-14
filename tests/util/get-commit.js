d27be6199495fd14a3de17a6564617d29c743fd0
ron litzenberger <ron.litzenberger@nearform>
2018-12-14 08:31:05 -0600

    Removed unnecessary wrapper function

--- tests/util/get-commit.js
+++ tests/util/get-commit.js
@@ -3,16 +3,12 @@
 const util = require('util')
 const execute = util.promisify(util.promisify(require('child_process').exec))
 
-const shell = async (command) => {
-  const out = await execute(command)
-  return out
-}
 const getIpfsCommit = async () => {
-  const out = await shell(`${__dirname}/getCommit.sh ${__dirname}/../../`)
+  const out = await execute(`${__dirname}/getCommit.sh ${__dirname}/../../`)
   return out.stdout.replace(/\n$/, '')
 }
 const getBranchName = async () => {
-  const out = await shell(`${__dirname}/getBranch.sh ${__dirname}/../../`)
+  const out = await execute(`${__dirname}/getBranch.sh ${__dirname}/../../`)
   return out.stdout.replace(/\n$/, '')
 }
 module.exports = {
