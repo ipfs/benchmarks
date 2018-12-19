876eb5df23e5a44d45411fd47cbb370e19daf65d
ron litzenberger <ron.litzenberger@nearform>
2018-12-18 21:50:28 -0600

    Replaced use of Promise with async/await in Go node

--- tests/lib/create-node.js
+++ tests/lib/create-node.js
@@ -6,31 +6,32 @@ const defaultConfig = require('../config/default-config.json')
 const defaultConfigGo = require('../config/default-config-go.json')
 const goConfigs = require('../config/go-configs.json')
 const fs = require('fs')
+const util = require('util')
+const fsMakeDir = util.promisify(fs.mkdir)
+const fsWriteFile = util.promisify(fs.writeFile)
+const rimrafPromise = util.promisify(rimraf)
 const privateKey = require('../config/private-key.json')
 const os = require('os')
 const conf = { tmpPath: os.tmpdir() }
 const { repoPath } = require('../package.json').config
 const ipfsClient = require('ipfs-http-client')
 const IPFSFactory = require('ipfsd-ctl')
-const util = require('util')
+
 const { once } = require('stream-iterators-utils')
 
-
-const initRepo = (path) => {
-  return new Promise((resolve, reject) => {
-    let init = spawn('ipfs', ['init'], { env: Object.assign(process.env, { IPFS_PATH: path }) })
-    init.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`)
-    })
-    init.stderr.on('data', (errorData) => {
-      console.error(`${errorData}`)
-      //return reject(Error(errorData))
-    })
-    init.on('close', (code, signal) => {
-      console.error('Repo iinitialized')
-      resolve(init)
-    })
+const initRepo = async (path) => {
+  let init = spawn('ipfs', ['init'], { env: Object.assign(process.env, { IPFS_PATH: path }) })
+  init.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`)
   })
+  init.stderr.on('data', (errorData) => {
+    console.error(`${errorData}`)
+  })
+  init.on('close', (code, signal) => {
+    console.log('Repo iinitialized')
+  })
+  await once(init, 'close')
+  return init
 }
 
 const CreateNodeJs = async (config, init, IPFS, count) => {
@@ -64,46 +65,46 @@ const CreateBrowser = async (config, init, IPFS, count) => {
 }
 
 const CreateGo = async (config, init, IPFS, count) => {
-  // TODO remove promise use 
-  return new Promise(async (resolve, reject) => {
-    const peerDir = `${conf.tmpPath}/ipfs${count}`
-    const peerSpecificConf = goConfigs[count]
-    const peerConf = Object.assign({}, defaultConfigGo, peerSpecificConf)
-    // todo make this async
-    rimraf.sync(peerDir)
-    // todo make this async
-    fs.mkdirSync(peerDir, { recursive: true })
-    await initRepo(peerDir)
-    // TODO make this async
-    fs.writeFileSync(`${peerDir}/config`, JSON.stringify(peerConf))
-    let peer = spawn('ipfs', ['daemon'], { env: Object.assign(process.env, { IPFS_PATH: peerDir }) })
-    peer.version = function () { return '1' }
-    peer.stdout.on('data', (data) => {
-      let version = {}
-      if (data.includes('go-ipfs version:')) {
-        const stdArray = data.toString('utf8').split('\n')
-        for (let item of stdArray) {
-          if (item.includes('go-ipfs version:')) {
-            version.version = item.split(':')[1].trim()
-          }
-          if (item.includes('Repo version:')) {
-            version.repo = item.split(':')[1].trim()
-          }
+  const peerDir = `${conf.tmpPath}/ipfs${count}`
+  const peerSpecificConf = goConfigs[count]
+  const peerConf = Object.assign({}, defaultConfigGo, peerSpecificConf)
+  await rimrafPromise(peerDir)
+  await fsMakeDir(peerDir, { recursive: true })
+  await initRepo(peerDir)
+  await fsWriteFile(`${peerDir}/config`, JSON.stringify(peerConf))
+  let peer = spawn('ipfs', ['daemon'], { env: Object.assign(process.env, { IPFS_PATH: peerDir }) })
+  peer.version = function () { return '1' }
+  peer.stdout.on('data', (data) => {
+    let version = {}
+    if (data.includes('go-ipfs version:')) {
+      const stdArray = data.toString('utf8').split('\n')
+      for (let item of stdArray) {
+        if (item.includes('go-ipfs version:')) {
+          version.version = item.split(':')[1].trim()
         }
-        peer.version = function () {
-          return version
+        if (item.includes('Repo version:')) {
+          version.repo = item.split(':')[1].trim()
         }
       }
-      if (data.includes('Daemon is ready')) resolve(peer)
-    })
-    peer.stderr.on('data', (data) => {
-      console.error(`${data}`)
-      return reject(Error(data))
-    })
-    peer.on('close', (code, signal) => {
-      console.error('Daemon is stopped')
-    })
+      peer.version = function () {
+        return version
+      }
+    }
+    if (data.includes('Daemon is ready')) {
+      peer.emit('done')
+    }
   })
+  peer.stderr.on('data', (data) => {
+    console.error(`${data}`)
+  })
+  peer.on('close', (code, signal) => {
+    console.error('Daemon is stopped')
+  })
+  peer.on('done', () => {
+    console.log('Daemon is ready')
+  })
+  await once(peer, 'done')
+  return peer
 }
 
 module.exports = {
