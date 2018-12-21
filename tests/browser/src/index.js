23ffefcc7d345c05c7a440f8f1062d9e52bbf7df
ron litzenberger <ron.litzenberger@nearform>
2018-12-21 09:33:14 -0600

    removed manifest and service worker

--- tests/browser/src/index.js
+++ tests/browser/src/index.js
@@ -1,14 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 
-//ReactDOM.render(<App />, document.getElementById('root'));
-import { HashRouter, Route, Switch, } from "react-router-dom";
-
-ReactDOM.render(<HashRouter><Switch> <Route exact path="/" component={App}/> </Switch></HashRouter>, document.getElementById('root'));
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<HashRouter><Switch> <Route exact path='/' component={App}/> </Switch></HashRouter>, document.getElementById('root'))
