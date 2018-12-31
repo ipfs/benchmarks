ba506f8ca36ec6a9bfc6863b42eb6bec0d146a66
Elexy <alex@seretalabs.com>
2018-12-31 11:54:31 -0500

    no need for Promise

--- runner/ipfs.js
+++ runner/ipfs.js
@@ -7,21 +7,21 @@ const fs = require('fs')
 const util = require('util')
 const fsStat = util.promisify(fs.stat)
 
-const store = (localpath) => {
-  return new Promise(async (resolve, reject) => {
-    if (config.ipfs.network.password) {
-      try {
-        await fsStat(localpath)
-        const shellCommand = `ipfs-cluster-ctl --host ${config.ipfs.network.address} --enc json --basic-auth ${config.ipfs.network.user}:${config.ipfs.network.password} add -r ${localpath}`
-        config.log.debug(shellCommand)
-        resolve(await local.run(shellCommand))
-      } catch (e) {
-        reject(e)
-      }
-    } else {
-      reject(Error('Env var IPFS_PASSWORD not set! Upload failed'))
+const store = async (localpath) => {
+  // return new Promise(async (resolve, reject) => {
+  if (config.ipfs.network.password) {
+    try {
+      await fsStat(localpath)
+      const shellCommand = `ipfs-cluster-ctl --host ${config.ipfs.network.address} --enc json --basic-auth ${config.ipfs.network.user}:${config.ipfs.network.password} add -r ${localpath}`
+      config.log.debug(shellCommand)
+      return await local.run(shellCommand)
+    } catch (e) {
+      throw e
     }
-  })
+  } else {
+    throw Error('Env var IPFS_PASSWORD not set! Upload failed')
+  }
+  // })
 }
 
 const parse = (outString, name) => {
