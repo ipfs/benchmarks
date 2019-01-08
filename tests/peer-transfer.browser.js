e9a849850690fdc1e7779237aada6ffb0e0a6f44
ron litzenberger <ron.litzenberger@nearform>
2019-01-08 17:22:02 -0600

    add peer transfer browser test to local runner

--- tests/peer-transfer.browser.js
+++ tests/peer-transfer.browser.js
@@ -4,14 +4,14 @@ const { build } = require('./schema/results')
 const run = require('./lib/runner')
 const { file } = require('./lib/fixtures')
 
-async function addMultiKbBrowser (node, name, warmup, fileSet, version) {
+async function peerTransferBrowser (node, name, warmup, fileSet, version) {
   const filePath = await file(fileSet)
   const page = node[0].page
   await page.reload()
-  const elementHandle = await page.$('.addLocalFile')
-  await elementHandle.uploadFile(...filePath)
-  const t = await page.waitFor('.addLocalFile_s_ready', { timeout: 100000 })
-  const element = await page.waitFor('.addLocalFile_ms_ready')
+  const elementHandle = await page.$('.peerTransfer')
+  await elementHandle.uploadFile(filePath)
+  const t = await page.waitFor('.peerTransfer_s_ready')
+  const element = await page.waitFor('.peerTransfer_ms_ready')
   const timeS = await page.evaluate(t => t.textContent, t)
   const timeMs = await page.evaluate(element => element.textContent, element)
   return build({
@@ -19,11 +19,11 @@ async function addMultiKbBrowser (node, name, warmup, fileSet, version) {
     warmup: 'off',
     file: filePath,
     meta: { version: version },
-    description: 'Add 100 1 KB files to local browser',
+    description: 'Transfer file between peers in the browser',
     file_set: fileSet,
     duration: { s: parseInt(timeS.split(':')[1]),
       ms: parseInt(timeMs.split(':')[1]) / 1000000 }
   })
 }
 
-run(addMultiKbBrowser, 1, 'browser')
+run(peerTransferBrowser, 1, 'browser')
