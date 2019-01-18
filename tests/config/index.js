419fe135a528eb2aa282af007b9cff526c3649a8
ron litzenberger <ron.litzenberger@nearform>
2019-01-18 07:25:22 -0600

    Update fileset for browser peer test

--- tests/config/index.js
+++ tests/config/index.js
@@ -27,13 +27,13 @@ const tests = { 'unixFsAdd': [{
   'fileSet': ['None'] }],
 'unixFsAddBrowser': [{
   'warmup': 'Off',
-  'fileSet': ['OneKBFile', 'OneMBFile'] }],
+  'fileSet': ['OneKBFile', 'OneMBFile', 'One4MBFile', 'One64MBFile'] }],
 'addMultiKbBrowser': [{
   'warmup': 'Off',
   'fileSet': ['Hundred1KBFile'] } ],
 'peerTransferBrowser': [{
   'warmup': 'Off',
-  'fileSet': ['OneKBFile', 'OneMBFile', 'One4MBFile'] }],
+  'fileSet': ['OneKBFile', 'OneMBFile'] }],
 'unixFsAddGo': [{
   'warmup': 'Off',
   'fileSet': ['OneKBFile', 'OneMBFile', 'One4MBFile', 'One64MBFile'] } ]
