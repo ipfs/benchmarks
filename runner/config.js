a42f4fe7d0621edd184e1373ab98417170810b8c
ron litzenberger <ron.litzenberger@nearform>
2019-02-05 01:35:17 -0600

    remove duplicate test from runner config

--- runner/config.js
+++ runner/config.js
@@ -175,10 +175,6 @@ const testAbstracts = [
     file: 'multi-peer-transfer.js -t ws -m mplex'
   },
   {
-    name: 'multiPeerTransfer_ws_mplex',
-    file: 'multi-peer-transfer.js -t ws -m mplex'
-  },
-  {
     name: 'multiPeerTransfer_tcp_mplex_secio',
     file: 'multi-peer-transfer.js -t tcp -m mplex -e secio'
   },
