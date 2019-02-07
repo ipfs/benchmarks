4c9a35d5a327f6eb91c39651969e1242375f3d9a
ron litzenberger <ron.litzenberger@nearform>
2019-02-07 12:41:26 -0600

    Update test docs

--- tests/add-multi-kb.browser.js
+++ tests/add-multi-kb.browser.js
@@ -3,7 +3,18 @@
 const { build } = require('./schema/results')
 const run = require('./lib/runner')
 const { file } = require('./lib/fixtures')
-
+/**
+ * Add many small files benchmark using IPFS api add in the browser.
+ * js0 -> js0 - A local test from one JS IPFS node to the same node
+ * @async
+ * @function addMultiKb
+ * @param {array} peerArray - An array of IPFS peers used during the test.
+ * @param {string} name - Name of the test used as sending results to the file with same name and data point in dashboard.
+ * @param {boolean} warmup - Not implemented.
+ * @param {string} fileSet - Describes file or list of files used for the test.
+ * @param {string} version - Version of IPFS used in benchmark.
+ * @return {Promise<Object>} The data from the benchamrk
+ */
 async function addMultiKbBrowser (node, name, warmup, fileSet, version) {
   const filePath = await file(fileSet)
   const page = node[0].page
@@ -19,7 +30,7 @@ async function addMultiKbBrowser (node, name, warmup, fileSet, version) {
     warmup: 'off',
     file: filePath,
     meta: { version: version },
-    description: 'Add many files (balanced)',
+    description: 'Add many files (local) js0 -> js0',
     file_set: fileSet,
     duration: { s: parseInt(timeS.split(':')[1]),
       ms: parseInt(timeMs.split(':')[1]) / 1000000 }
